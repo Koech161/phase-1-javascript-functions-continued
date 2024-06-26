01f3ab90a18db8f98847f825bcb1f14331c664c9
Abel Koech <koechabel161@gmail.com>
2024-06-26 17:29:30 +0300

    continued

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(action = 'roller-skate'){
+ return `This Saturday, I want to ${action}!`;
+}
+function mondayWork(action= 'go to the office'){
+    return `This Monday, I will ${action}.`;
+}
+function wrapAdjective(emphatic= '*'){
+    return function(action='a hard worker'){
+        return `You are ${emphatic}${action}${emphatic}!`
+    } 
+}
